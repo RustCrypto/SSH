b872531ab2b90302411c49d00a8631d5c982547b
Tony Arcieri <bascule@gmail.com>
2023-03-06 18:54:52 -0700

    ssh-key: initial `aes256-gcm@openssh.com` support
    Adds support for OpenSSH private keys which have been encrypted with
    AES-256-GCM.
    
    The test example key was generated by encrypting `id_ed25519` with:
    
        $ ssh-keygen -Z aes256-gcm@openssh.com -p -f id_ed25519
    
    Decryption and encryption are supported when the optional `aes-gcm`
    feature is enabled.

--- ssh-key/tests/encrypted_private_key.rs
+++ ssh-key/tests/encrypted_private_key.rs
@@ -9,18 +9,23 @@ use ssh_key::{Algorithm, Cipher, Kdf, KdfAlg, PrivateKey};
 #[cfg(all(feature = "encryption"))]
 const OPENSSH_ED25519_EXAMPLE: &str = include_str!("examples/id_ed25519");
 
-/// Encrypted Ed25519 OpenSSH-formatted private key.
+/// AES-CTR encrypted Ed25519 OpenSSH-formatted private key.
 ///
-/// This is the encrypted form of `OPENSSH_ED25519_EXAMPLE`.
-const OPENSSH_ED25519_ENC_EXAMPLE: &str = include_str!("examples/id_ed25519.enc");
+/// Plaintext is `OPENSSH_ED25519_EXAMPLE`.
+const OPENSSH_AES_CTR_ED25519_EXAMPLE: &str = include_str!("examples/id_ed25519.aes-ctr.enc");
+
+/// AES-GCM encrypted Ed25519 OpenSSH-formatted private key.
+///
+/// Plaintext is `OPENSSH_ED25519_EXAMPLE`.
+const OPENSSH_AES_GCM_ED25519_EXAMPLE: &str = include_str!("examples/id_ed25519.aes-gcm.enc");
 
 /// Bad password; don't actually use outside tests!
 #[cfg(all(feature = "encryption"))]
 const PASSWORD: &[u8] = b"hunter42";
 
 #[test]
-fn decode_ed25519_enc_openssh() {
-    let key = PrivateKey::from_openssh(OPENSSH_ED25519_ENC_EXAMPLE).unwrap();
+fn decode_openssh_aes_ctr() {
+    let key = PrivateKey::from_openssh(OPENSSH_AES_CTR_ED25519_EXAMPLE).unwrap();
     assert_eq!(Algorithm::Ed25519, key.algorithm());
     assert_eq!(Cipher::Aes256Ctr, key.cipher());
     assert_eq!(KdfAlg::Bcrypt, key.kdf().algorithm());
@@ -39,10 +44,42 @@ fn decode_ed25519_enc_openssh() {
     );
 }
 
+#[test]
+fn decode_openssh_aes_gcm() {
+    let key = PrivateKey::from_openssh(OPENSSH_AES_GCM_ED25519_EXAMPLE).unwrap();
+    assert_eq!(Algorithm::Ed25519, key.algorithm());
+    assert_eq!(Cipher::Aes256Gcm, key.cipher());
+    assert_eq!(KdfAlg::Bcrypt, key.kdf().algorithm());
+
+    match key.kdf() {
+        Kdf::Bcrypt { salt, rounds } => {
+            assert_eq!(salt, &hex!("11bdc133ef64644115b176917e47cbaf"));
+            assert_eq!(*rounds, 16);
+        }
+        other => panic!("unexpected KDF algorithm: {:?}", other),
+    }
+
+    assert_eq!(
+        &hex!("b33eaef37ea2df7caa010defdea34e241f65f1b529a4f43ed14327f5c54aab62"),
+        key.public_key().key_data().ed25519().unwrap().as_ref(),
+    );
+}
+
 #[cfg(all(feature = "encryption"))]
 #[test]
-fn decrypt_ed25519_enc_openssh() {
-    let key_enc = PrivateKey::from_openssh(OPENSSH_ED25519_ENC_EXAMPLE).unwrap();
+fn decrypt_openssh_aes_ctr() {
+    let key_enc = PrivateKey::from_openssh(OPENSSH_AES_CTR_ED25519_EXAMPLE).unwrap();
+    let key_dec = key_enc.decrypt(PASSWORD).unwrap();
+    assert_eq!(
+        PrivateKey::from_openssh(OPENSSH_ED25519_EXAMPLE).unwrap(),
+        key_dec
+    );
+}
+
+#[cfg(all(feature = "aes-gcm"))]
+#[test]
+fn decrypt_openssh_aes_gcm() {
+    let key_enc = PrivateKey::from_openssh(OPENSSH_AES_GCM_ED25519_EXAMPLE).unwrap();
     let key_dec = key_enc.decrypt(PASSWORD).unwrap();
     assert_eq!(
         PrivateKey::from_openssh(OPENSSH_ED25519_EXAMPLE).unwrap(),
@@ -51,21 +88,57 @@ fn decrypt_ed25519_enc_openssh() {
 }
 
 #[test]
-fn encode_ed25519_enc_openssh() {
-    let key = PrivateKey::from_openssh(OPENSSH_ED25519_ENC_EXAMPLE).unwrap();
+fn encode_openssh_aes_ctr() {
+    let key = PrivateKey::from_openssh(OPENSSH_AES_CTR_ED25519_EXAMPLE).unwrap();
     assert_eq!(
-        OPENSSH_ED25519_ENC_EXAMPLE.trim_end(),
+        OPENSSH_AES_CTR_ED25519_EXAMPLE.trim_end(),
+        key.to_openssh(Default::default()).unwrap().trim_end()
+    );
+}
+
+#[test]
+fn encode_openssh_aes_gcm() {
+    let key = PrivateKey::from_openssh(OPENSSH_AES_GCM_ED25519_EXAMPLE).unwrap();
+    assert_eq!(
+        OPENSSH_AES_GCM_ED25519_EXAMPLE.trim_end(),
         key.to_openssh(Default::default()).unwrap().trim_end()
     );
 }
 
 #[cfg(all(feature = "encryption", feature = "getrandom"))]
 #[test]
-fn encrypt_ed25519_openssh() {
+fn encrypt_openssh_aes_ctr() {
     use rand_core::OsRng;
 
     let key_dec = PrivateKey::from_openssh(OPENSSH_ED25519_EXAMPLE).unwrap();
     let key_enc = key_dec.encrypt(&mut OsRng, PASSWORD).unwrap();
+
+    // Ensure encrypted key round trips through encoder/decoder
+    let key_enc_str = key_enc.to_openssh(Default::default()).unwrap();
+    let key_enc2 = PrivateKey::from_openssh(&*key_enc_str).unwrap();
+    assert_eq!(key_enc, key_enc2);
+
+    // Ensure decrypted key matches the original
+    let key_dec2 = key_enc.decrypt(PASSWORD).unwrap();
+    assert_eq!(key_dec, key_dec2);
+}
+
+#[cfg(all(feature = "aes-gcm", feature = "getrandom"))]
+#[test]
+fn encrypt_openssh_aes_gcm() {
+    use rand_core::{OsRng, RngCore};
+
+    let key_dec = PrivateKey::from_openssh(OPENSSH_ED25519_EXAMPLE).unwrap();
+
+    let checkint = RngCore::next_u32(&mut OsRng);
+    let key_enc = key_dec
+        .encrypt_with(
+            Cipher::Aes256Gcm,
+            Kdf::new(Default::default(), &mut OsRng).unwrap(),
+            checkint,
+            PASSWORD,
+        )
+        .unwrap();
 
     // Ensure encrypted key round trips through encoder/decoder
     let key_enc_str = key_enc.to_openssh(Default::default()).unwrap();
