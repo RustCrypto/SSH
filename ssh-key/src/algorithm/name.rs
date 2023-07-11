8d739884ca6f2efa91dc821780aaaefbd461ea7e
Gabriela Moldovan <gabi@torproject.org>
2023-07-11 12:02:04 +0100

    ssh-key: remove the `tinystr` dependency.
    This replaces the `tinystr` dependency with a hand-rolled `AsciiStr`
    implementation.

--- ssh-key/src/algorithm/name.rs
+++ ssh-key/src/algorithm/name.rs
@@ -1,6 +1,6 @@
-use core::str;
+use core::ops::Deref;
+use core::str::{self, FromStr};
 use encoding::LabelError;
-use tinystr::TinyAsciiStr;
 
 /// The suffix added to the `name` in a `name@domainname` algorithm string identifier.
 const CERT_STR_SUFFIX: &str = "-cert-v01";
@@ -16,6 +16,39 @@ const MAX_ALGORITHM_NAME_LEN: usize = 64;
 /// algorithm name).
 const MAX_CERT_STR_LEN: usize = MAX_ALGORITHM_NAME_LEN + CERT_STR_SUFFIX.len();
 
+#[derive(Copy, Clone, Debug, Eq, Hash, PartialEq, PartialOrd, Ord)]
+struct AsciiStr<const N: usize> {
+    inner: [u8; N],
+    len: usize,
+}
+
+impl<const N: usize> FromStr for AsciiStr<N> {
+    type Err = LabelError;
+
+    fn from_str(id: &str) -> Result<Self, LabelError> {
+        if id.len() > N || !id.is_ascii() {
+            return Err(LabelError::new(id));
+        }
+
+        let len = id.len();
+        let mut inner = [0u8; N];
+        inner[..len].copy_from_slice(id.as_bytes());
+
+        Ok(Self { inner, len })
+    }
+}
+
+impl<const N: usize> Deref for AsciiStr<N> {
+    type Target = str;
+    #[inline]
+    fn deref(&self) -> &str {
+        // This conversion should **not** fail, as `self.inner` can only ever be constructed from
+        // valid `&str`s.
+        str::from_utf8(&self.inner[..self.len])
+            .expect("AsciiStr can only be built from valid strings")
+    }
+}
+
 /// A string representing an additional algorithm name in the `name@domainname` format (see
 /// [RFC4251 § 6]).
 ///
@@ -30,16 +63,16 @@ const MAX_CERT_STR_LEN: usize = MAX_ALGORITHM_NAME_LEN + CERT_STR_SUFFIX.len();
 ///
 /// [RFC4251 § 6]: https://www.rfc-editor.org/rfc/rfc4251.html#section-6
 //
-// NOTE: We use TinyAsciiStr instead of String to allow Algorithm to implement Copy.
+// NOTE: We use AsciiStr instead of String to allow Algorithm to implement Copy.
 #[derive(Copy, Clone, Debug, Eq, Hash, PartialEq, PartialOrd, Ord)]
 pub struct AlgorithmName {
     /// The string identifier which corresponds to this algorithm.
-    id: TinyAsciiStr<MAX_ALGORITHM_NAME_LEN>,
+    id: AsciiStr<MAX_ALGORITHM_NAME_LEN>,
     /// The string identifier which corresponds to the OpenSSH certificate format.
     ///
     /// This is derived from the algorithm name by inserting `"-cert-v01"` immediately after the
     /// name preceding the at-symbol (`@`).
-    certificate_str: TinyAsciiStr<MAX_CERT_STR_LEN>,
+    certificate_str: AsciiStr<MAX_CERT_STR_LEN>,
 }
 
 impl AlgorithmName {
@@ -55,11 +88,7 @@ impl AlgorithmName {
 
     /// Create a new [`AlgorithmName`] from an OpenSSH certificate format string identifier.
     pub fn from_certificate_str(id: &str) -> Result<Self, LabelError> {
-        if id.len() > MAX_CERT_STR_LEN {
-            return Err(LabelError::new(id));
-        }
-
-        let certificate_str = TinyAsciiStr::from_str(id).map_err(|_| LabelError::new(id))?;
+        let certificate_str = AsciiStr::from_str(id)?;
 
         // Derive the algorithm name from the certificate format string identifier:
         let (name, domain) = id.split_once('@').ok_or_else(|| LabelError::new(id))?;
@@ -73,8 +102,7 @@ impl AlgorithmName {
             .strip_suffix(CERT_STR_SUFFIX)
             .ok_or_else(|| LabelError::new(id))?;
 
-        let algorithm_name =
-            TinyAsciiStr::from_str(&format!("{name}@{domain}")).map_err(|_| LabelError::new(id))?;
+        let algorithm_name = AsciiStr::from_str(&format!("{name}@{domain}"))?;
 
         Ok(Self {
             id: algorithm_name,
@@ -83,15 +111,11 @@ impl AlgorithmName {
     }
 }
 
-impl str::FromStr for AlgorithmName {
+impl FromStr for AlgorithmName {
     type Err = LabelError;
 
     fn from_str(id: &str) -> Result<Self, LabelError> {
-        if id.len() > MAX_ALGORITHM_NAME_LEN {
-            return Err(LabelError::new(id));
-        }
-
-        let algorithm_name = TinyAsciiStr::from_str(id).map_err(|_| LabelError::new(id))?;
+        let algorithm_name = AsciiStr::from_str(id)?;
 
         // Derive the certificate format string identifier from the algorithm name:
         let (name, domain) = id.split_once('@').ok_or_else(|| LabelError::new(id))?;
@@ -101,8 +125,7 @@ impl str::FromStr for AlgorithmName {
             return Err(LabelError::new(id));
         }
 
-        let certificate_str = TinyAsciiStr::from_str(&format!("{name}{CERT_STR_SUFFIX}@{domain}"))
-            .map_err(|_| LabelError::new(id))?;
+        let certificate_str = AsciiStr::from_str(&format!("{name}{CERT_STR_SUFFIX}@{domain}"))?;
 
         Ok(Self {
             id: algorithm_name,
