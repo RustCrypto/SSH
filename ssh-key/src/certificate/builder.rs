f8760a5fbfbc00b0dc68f93d87e09736afbd2804
Tony Arcieri <bascule@gmail.com>
2024-03-29 13:17:52 -0600

    ssh-key: use `u64` type for certificate timestamps
    Closes #174
    
    Previously certificates only supported the `i64` range to allow for
    infallible conversions to/from `SystemTime`.
    
    Unfortunately OpenSSH defaults to using `u64::MAX` as the `valid_before`
    time in order to represent certificate that's valid "forever". The
    previous restriction meant that `ssh-key` was incapible of parsing such
    certificates.
    
    This commit switches to using a raw `u64` everywhere, and changing
    conversions to `SystemTime` to return an `Option<SystemTime>` which is
    `None` if the `u64` value overflows an `i64`.

--- ssh-key/src/certificate/builder.rs
+++ ssh-key/src/certificate/builder.rs
@@ -1,6 +1,6 @@
 //! OpenSSH certificate builder.
 
-use super::{unix_time::UnixTime, CertType, Certificate, Field, OptionsMap};
+use super::{CertType, Certificate, Field, OptionsMap};
 use crate::{public, Result, Signature, SigningKey};
 use alloc::{string::String, vec::Vec};
 
@@ -8,7 +8,7 @@ use alloc::{string::String, vec::Vec};
 use rand_core::CryptoRngCore;
 
 #[cfg(feature = "std")]
-use std::time::SystemTime;
+use {super::UnixTime, std::time::SystemTime};
 
 #[cfg(doc)]
 use crate::PrivateKey;
@@ -84,8 +84,8 @@ pub struct Builder {
     cert_type: Option<CertType>,
     key_id: Option<String>,
     valid_principals: Option<Vec<String>>,
-    valid_after: UnixTime,
-    valid_before: UnixTime,
+    valid_after: u64,
+    valid_before: u64,
     critical_options: OptionsMap,
     extensions: OptionsMap,
     comment: Option<String>,
@@ -105,12 +105,6 @@ impl Builder {
         valid_after: u64,
         valid_before: u64,
     ) -> Result<Self> {
-        let valid_after =
-            UnixTime::new(valid_after).map_err(|_| Field::ValidAfter.invalid_error())?;
-
-        let valid_before =
-            UnixTime::new(valid_before).map_err(|_| Field::ValidBefore.invalid_error())?;
-
         if valid_before < valid_after {
             return Err(Field::ValidBefore.invalid_error());
         }
@@ -304,7 +298,7 @@ impl Builder {
 
         #[cfg(debug_assertions)]
         cert.validate_at(
-            cert.valid_after.into(),
+            cert.valid_after,
             &[cert.signature_key.fingerprint(Default::default())],
         )?;
 
